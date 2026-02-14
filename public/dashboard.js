c3b1e3cccaba7a8764081201ffa6a475a3c7e51f
Varzzz123 <varzgeteamabar@gmail.com>
2026-02-14 19:13:46 +0700

    Add files via upload

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,29 +1,20 @@
 const p = JSON.parse(localStorage.getItem('selectedChar'));
 if (!p) window.location.href = 'index.html';
 
-document.getElementById('displayName').innerText = p.username.replace('_', ' ');
-document.getElementById('displayUcp').innerText = `UCP: ${p.ucp}`;
+        // Set Data
 document.getElementById('skinImg').src = `https://night7.com/skins/${p.skin}.png`;
+document.getElementById('sLevel').innerText = p.level;
+document.getElementById('sExp').innerText = `${p.levelup.toLocaleString()} / ${(p.level * 8).toLocaleString()}`;
+document.getElementById('sLastLogin').innerText = p.last_login;
 
-document.getElementById('sGender').innerText = (p.gender == 2) ? "Female" : "Male";
-document.getElementById('sMoney').innerText = `$${p.money.toLocaleString()}`;
-document.getElementById('sBank').innerText = `$${p.bmoney.toLocaleString()}`;
-document.getElementById('sRek').innerText = p.brek;
-document.getElementById('sPhone').innerText = p.phone;
-document.getElementById('sAge').innerText = p.age;
+        // Bars Logic
+document.getElementById('vHealth').innerText = Math.round(p.health) + "%";
+document.getElementById('bHealth').style.width = p.health + "%";
+        
+document.getElementById('vEnergy').innerText = p.energy + "%";
+document.getElementById('bEnergy').style.width = p.energy + "%";
 
-const factions = { 1: "San Andreas Police", 2: "San Andreas Goverment", 3: "San Andreas Medic", 4: "San Andreas News" };
-document.getElementById('sFaction').innerText = factions[p.faction] || "None";
-document.getElementById('sFamily').innerText = (p.family != -1) ? `Family ID: ${p.family}` : "None";
+document.getElementById('vStress').innerText = p.stress + "%";
+document.getElementById('bStress').style.width = p.stress + "%";
 
-document.getElementById('sUid').innerText = p.reg_id;
-document.getElementById('sLevelExp').innerText = `${p.levelup}/${p.level * 8}`;
-document.getElementById('sWarn').innerText = `${p.warn}/20`;
-document.getElementById('sGold').innerText = p.gold;
-document.getElementById('sVip').innerText = p.vip > 0 ? `Level ${p.vip}` : "None";
-document.getElementById('sTime').innerText = `${p.hours}h ${p.minutes}m ${p.seconds}s`;
-
-function logout() {
-    localStorage.clear();
-    window.location.href = 'index.html';
-}
+function logout() { localStorage.clear(); window.location.href = 'index.html'; }
